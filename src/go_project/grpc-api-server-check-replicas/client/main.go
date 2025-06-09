20425cde435fee9cd3767d7e9dd130ab610d249f
Edmond Guo <edmondandy@gmail.com>
2025-06-09 15:28:58 +1000

    update

--- src/go_project/grpc-api-server-check-replicas/client/main.go
+++ src/go_project/grpc-api-server-check-replicas/client/main.go
@@ -3,22 +3,35 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	pb "go_grpc/proto"
 )
 
 func main() {
-
-	conn, _ := grpc.NewClient("localhost:50051", grpc.WithInsecure())
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("localhost:50051", opts...)
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
 	defer conn.Close()
 
 	client := pb.NewReplicaServiceClient(conn)
-	resp, _ := client.GetDeploymentReplicas(context.TODO(), &pb.ReplicaRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	resp, err := client.GetDeploymentReplicas(ctx, &pb.ReplicaRequest{
 		Namespace: "default",
 		Name:      "nginx-deployment",
 	})
-	fmt.Println("Available Replicas:", resp.AvailableReplicas)
+	if err != nil {
+		log.Fatalf("Error calling GetDeploymentReplicas: %v", err)
+	}
 
+	fmt.Println("Name", resp.Name)
+	fmt.Println("Available Replicas:", resp.AvailableReplicas)
 }
--- src/go_project/grpc-api-server-check-replicas/proto/README.md
+++ src/go_project/grpc-api-server-check-replicas/proto/README.md
@@ -1,1 +1,8 @@
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/*.proto
+
+https://protobuf.dev/reference/go/go-generated/#package
+https://google.github.io/proto-lens/installing-protoc.html
+
