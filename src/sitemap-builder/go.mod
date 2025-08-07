93f09202069d5ef43c07a4ed46f24bc10f0dec7f
Edmond Guo <edmond.guo@edg.com.au>
2025-08-07 10:07:28 +1000

    update sitemap-builder

--- src/sitemap-builder/go.mod
+++ src/sitemap-builder/go.mod
@@ -1,3 +1,7 @@
 module github.com/edmondandy/go_project/src/sitemap-builder
 
 go 1.23.4
+
+require github.com/edmondandy/go_project/src/html-link-parser v0.0.0-20250806232206-fd4e25199930
+
+require golang.org/x/net v0.42.0 // indirect
--- src/sitemap-builder/main.go
+++ src/sitemap-builder/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 
 	link "github.com/edmondandy/go_project/src/html-link-parser"
 )
@@ -19,7 +21,7 @@ import (
 */
 
 func main() {
-	urlFlag := flag.String("url", "https://gophercises.com", "The URL to fetch the sitemap from")
+	urlFlag := flag.String("url", "http://gophercises.com", "The URL to fetch the sitemap from")
 	flag.Parse()
 
 	fmt.Println(*urlFlag)
@@ -28,10 +30,34 @@ func main() {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	
+
+/*
+   /some-path - add domain to include full url
+   https://gophercises.com/some-path - use it as is
+   #fragment - ignore it
+   mailto:jon@calhoun.io - ignore it
+*/
+	reqUrl := resp.Request.URL
+	fmt.Println(reqUrl.String())
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
 	links, _ := link.Parse(resp.Body)
+	var hrefs []string
 	for _, l := range links {
-		fmt.Printf("Found link: %s (%s)\n", l.Text, l.Href)
+		switch {
+			case strings.HasPrefix(l.Href, "/"):
+				hrefs = append(hrefs, base+l.Href)
+			case strings.HasPrefix(l.Href, "http"):
+				hrefs = append(hrefs, l.Href)
+			// default:
+			// 	fmt.Printf("we are skipping this link: %s\n", l.Href)
+		}
+	}
+	for _, href := range hrefs {
+		fmt.Println(href)
 	}
 }
-
